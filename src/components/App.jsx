ecb79a575da3d3a9a74db2fb76dfcdba54cb3076
Gaurav Kumar <49734163+gauravkumar34@users.noreply.github.com>
2020-08-29 04:45:53 +0530

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,50 @@
+import React, { useState } from "react";
+
+function App() {
+  const [fullName, setFullName] = useState({
+    fName: "",
+    lName: ""
+  });
+  function handleCh(event) {
+    const { value, name } = event.target;
+
+    setFullName((prevValue) => {
+      if (name === "fName") {
+        return {
+          fName: value,
+          lName: prevValue.lName
+        };
+      } else if (name === "lName") {
+        return {
+          fName: prevValue.fName,
+          lName: value
+        };
+      }
+    });
+  }
+
+  return (
+    <div className="container">
+      <h1>
+        Hello {fullName.fName} {fullName.lName}
+      </h1>
+      <form>
+        <input
+          onChange={handleCh}
+          name="fName"
+          placeholder="First Name"
+          value={fullName.fname}
+        />
+        <input
+          onChange={handleCh}
+          name="lName"
+          placeholder="Last Name"
+          value={fullName.lname}
+        />
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+}
+
+export default App;
